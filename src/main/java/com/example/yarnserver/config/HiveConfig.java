d3b713202cf058f927c9708b06dcdbab5a12066c
王恩堂 <1611266133@qq.com>
2023-10-19 23:29:34 +0800

    增加Hive配置类

--- src/main/java/com/example/yarnserver/config/HiveConfig.java
+++ src/main/java/com/example/yarnserver/config/HiveConfig.java
@@ -1,4 +1,28 @@
 package com.example.yarnserver.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Configuration
 public class HiveConfig {
+    @Value("${Hive.url}")
+    private String HIVE_URL;
+    @Value("${Hive.driver}")
+    private String HIVE_DRIVER;
+
+    @Bean
+    public Connection createHiveConnection() {
+        Connection hiveConnection = null;
+        try {
+            hiveConnection = DriverManager.getConnection(HIVE_URL, "", "");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return hiveConnection;
+    }
 }
--- src/main/java/com/example/yarnserver/service/HiveService.java
+++ src/main/java/com/example/yarnserver/service/HiveService.java
@@ -2,6 +2,30 @@ package com.example.yarnserver.service;
 
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 @Service
 public class HiveService {
+    @Resource
+    private Connection hiveConnection;
+
+    public ResultSet executeSql(String sql) {
+        Statement statement = null;
+        try {
+            statement = hiveConnection.createStatement();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        ResultSet resultSet = null;
+        try {
+            resultSet = statement.executeQuery(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return resultSet;
+    }
 }
